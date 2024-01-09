e00b746db97743fea5bbc75095c8f3cd92d6d951
drcd <drcd3@hotmail.com>
2024-01-09 16:39:50 +0100

    Fix small issue

--- jest-preset.js
+++ jest-preset.js
@@ -1,4 +1,7 @@
-import {resolve} from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
   globalSetup: resolve(__dirname, './setup.js'),
